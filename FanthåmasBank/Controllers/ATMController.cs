acf9761f150a50d77c9fc74cb358c4cac7888d59
thomas lotsander <thomaslotsander@gmail.com>
2018-11-08 00:16:12 +0100

    Added ATM Controller Added ATMViewModel Added View and frontend logic

---
+++ FanthåmasBank/Controllers/ATMController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FanthåmasBank.Models;
+using FanthåmasBank.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FanthåmasBank.Controllers
+{
+    public class ATMController : Controller
+    {
+        public IActionResult Index()
+        {
+            ATMViewModel model = new ATMViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public void Withdraw(ATMViewModel model)
+        {
+            AllCustomers instance = AllCustomers.Instance();
+
+            var account = instance.Customers.Select(c => c.Accounts.FirstOrDefault(x => x.AccountNumber.ToString() == model.AccountNumber)).FirstOrDefault();
+
+            BankRepository reo = new BankRepository();
+            reo.Withdraw(account, model.Amount);
+        }
+
+        public void Deposit(string accountNumberm, decimal amount)
+        {
+
+        }
+    }
+}
---
+++ FanthåmasBank/Models/ViewModels/ATMViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FanthåmasBank.Models.ViewModels
+{
+    public class ATMViewModel
+    {
+        public decimal Amount { get; set; }
+        public string AccountNumber { get; set; }
+    }
+}
