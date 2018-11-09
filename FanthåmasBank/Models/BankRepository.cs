43bc17ccf1648329aaff88fc3c5c21ee83505327
Mikael Hietala <Mikael.Hietala@yh.nackademin.se>
2018-11-09 17:20:39 +0100

    Created Transform Nethod and moved GetAccount from ATM Controller to this class insted.

--- FanthåmasBank/Models/BankRepository.cs
+++ FanthåmasBank/Models/BankRepository.cs
@@ -26,5 +26,44 @@ namespace FanthåmasBank.Models
             account.Amount += amount;
             return account.Amount;
         }
+
+
+        public string Transfer(Account senderAccount, Account receiverAccount, decimal amount)
+        {
+            if (amount > 0)
+            {
+                var senderAccountAmount_beforeWithdraw = senderAccount.Amount;
+                var newSenderAccountAmount = Withdraw(senderAccount, amount);
+
+                if (newSenderAccountAmount == (senderAccountAmount_beforeWithdraw - amount))
+                {
+                    var newReceiverAccountAmount = Deposit(receiverAccount, amount);
+                    return $"Sender:{newSenderAccountAmount},Receiver:{newReceiverAccountAmount}";
+                }
+            }
+            return $"Sender:{senderAccount.Amount},Receiver:{receiverAccount.Amount}";
+        }
+
+        public Account GetAccount(string accountNumber)
+        {
+            AllCustomers instance = AllCustomers.Instance();
+            List<Account> accounts = new List<Account>();
+            Account account = null;
+
+            foreach (var item in instance.Customers)
+            {
+                foreach (var acc in item.Accounts)
+                {
+                    if (acc.AccountNumber == accountNumber)
+                    {
+                        account = acc;
+                    }
+                }
+            }
+            // Tips på hur jag kan one-lina båda foreach-looparna uppskattas. 
+            // Account account = instance.Customers.Select(c => c.Accounts.FirstOrDefault(x => x.AccountNumber == accountNumber)).FirstOrDefault();
+
+            return account;
+        }
     }
 }
